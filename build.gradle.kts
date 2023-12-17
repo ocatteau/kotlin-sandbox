36008066d01a91228cdbd17399b356c3deed20c8
Olivier Catteau <ocatteau@onewealthplace.com>
2023-12-17 22:19:22 +0100

    Bootstrap

---
+++ build.gradle.kts
@@ -0,0 +1,75 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.spring") version "1.9.20"
+}
+
+group = "fr.ocat.sandbox"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+
+val jacksonVersion = "2.14.1"
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.slf4j:slf4j-jdk14:1.7.36")
+
+    implementation(platform("org.http4k:http4k-bom:5.6.0.0"))
+    implementation("org.http4k:http4k-core")
+    implementation("org.http4k:http4k-client-apache")
+    implementation("org.http4k:http4k-template-handlebars")
+    implementation("org.http4k:http4k-client-apache")
+
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-parameter-names:$jacksonVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:$jacksonVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.strikt:strikt-core:0.34.0")
+    testImplementation("org.http4k:http4k-testing-approval")
+    testImplementation("org.http4k:http4k-testing-hamkrest")
+    testImplementation("org.http4k:http4k-testing-strikt")
+}
+
+
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+tasks.compileKotlin {
+    compilerOptions.freeCompilerArgs.add("-Xcontext-receivers")
+}
+
+tasks.compileTestKotlin {
+    compilerOptions.freeCompilerArgs.add("-Xcontext-receivers")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
